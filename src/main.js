92efacb43a859377ec04700ba25a8ef637cf0220
Jiafu <jiafu20001003@gmail.com>
2023-12-28 16:26:23 +0800

    製作登入頁面

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { createApp } from 'vue';
+// 匯入axios套件,連接api使用
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+// 加入axios套件
+app.use(VueAxios, axios);
+app.mount('#app');
---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vuejs-accessibility/no-autofocus -->
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<template>
+  <div class="container">
+    <form class="row justify-content-center" @submit.prevent="signIn">
+      <div class="col-md-6">
+        <h3 class="mb-3">請先登入</h3>
+        <div class="mb-2">
+          <!-- sr-only提供盲人或弱勢族群可用螢幕閱讀器 -->
+          <label for="inputEmail" class="sr-only">Email address</label>
+          <input
+            type="email"
+            id="inputEmail"
+            class="form-control"
+            placeholder="Email address"
+            required
+            autofocus
+            v-model="user.username"
+          />
+        </div>
+        <div class="mb-2">
+          <!-- v-model與data連接 -->
+          <label for="inputPassword" class="sr-only">Password</label>
+          <input
+            type="password"
+            id="inputPassword"
+            class="form-control"
+            placeholder="Password"
+            required
+            v-model="user.password"
+          />
+        </div>
+        <div class="text-end mt-4">
+          <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    signIn() {
+      // 串接api使用$http取得axios的方法
+      const api = `${process.env.VUE_APP_API}admin/signin`;
+      this.$http.post(api, this.user).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
